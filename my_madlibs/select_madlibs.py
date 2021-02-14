b5bb2c741225ad60f4c40ba9c7ca7e713f877981
Nguyen Dai Truong Thanh <36881258+daitng17@users.noreply.github.com>
2021-02-14 20:24:11 +0900

    commit on 'main'

--- my_madlibs/select_madlibs.py
+++ my_madlibs/select_madlibs.py
@@ -1,4 +1,12 @@
 # randomize the collection of madlibs for players
 
 from madlibs_collection import simple, business
+import random
+
+def main():
+    select_madlib = random.choice([simple, business])
+    select_madlib.madlib()
+
+if __name__ == '__main__':
+    main()
 
